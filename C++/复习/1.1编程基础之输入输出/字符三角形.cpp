471bfb83004c0a55855925fb872b0371ffd968ac
max777xam <max7777777xam@163.com>
2025-06-28 11:49:12 +0800

    字符三角形

---
+++ C++/复习/1.1编程基础之输入输出/字符三角形.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char x;
+    scanf("%c", &x);
+    printf("  %c  \n %c%c%c \n%c%c%c%c%c", x, x, x, x, x, x, x, x, x);
+    return 0;
+}
