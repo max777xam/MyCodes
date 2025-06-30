6cb5193ba3306e0e2d0168bef73e53ee0fc65b04
max777xam <max7777777xam@163.com>
2025-06-30 20:55:55 +0800

    hello

---
+++ C++/复习/1.2编程基础之变量定义、赋值及转换/打印ASCII码.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a;
+    scanf("%c", &a);
+    int ans = a;
+    cout << ans << endl;
+    return 0;
+}
---
+++ C++/复习/1.2编程基础之变量定义、赋值及转换/浮点数向零舍入.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a;
+    scanf("%f", &a);
+    int ans;
+    ans = a;
+    printf("%d", ans);
+    return 0;
+}
