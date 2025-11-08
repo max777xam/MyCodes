bbf9e4877e418142971302d91c08391694a38359
max777xam <max7777777xam@163.com>
2025-11-08 16:10:51 +0800

    ,

--- C++/南工程/数组/找鞍点.c
+++ C++/南工程/数组/找鞍点.c
@@ -16,15 +16,26 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         int max = a[i][1];
+        int flag;
         for (int j = 1; j <= n; j++)
         {
             if (a[i][j] > max)
             {
                 max = a[i][j];
+                flag = j;
             }
+            int check = 1;
             for (int k = 1; k <= n; k++)
             {
-                
+                if (a[k][flag] < a[i][j])
+                {
+                    check = 0;
+                    break;
+                }
+            }
+            if (check == 1)
+            {
+                printf("第%d行,第%d列为鞍点\n", i, flag);
             }
         }
     }
